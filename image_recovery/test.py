1ac73276abd2981e95cf39888968afbff0aeb533
bilalkutuk <bilalkutuk@gmail.com>
2021-05-31 17:30:52 +0300

    Update test.py

--- image_recovery/test.py
+++ image_recovery/test.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('g3.jpeg',cv2.IMREAD_GRAYSCALE)
-median = cv2.medianBlur(img, 3)
+img = cv2.imread('image_recovery/s4.jpeg')
+print(img)
+median = cv2.medianBlur(img, 5)
 compare = np.concatenate((img, median), axis=1) #side by side comparison
 
 cv2.imshow('img', compare)
